0d22aee3dd21e5187202466da5300948a9b68751
王旭 <1947147481@qq.com>
2022-06-22 20:55:12 +0800

    调换插件顺序

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    vue(),
   ],
 });
