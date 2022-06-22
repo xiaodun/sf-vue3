ce08db4b174289ffc47e453f30d7ad632e5edd2c
王旭 <1947147481@qq.com>
2022-06-22 16:32:00 +0800

    关于ElMessage样式不生效

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,19 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
+import { h } from "vue";
+function showMessage() {
+  ElMessage({
+    title: "Title",
+    message: h("i", { style: "color: teal" }, "This is a reminder"),
+  });
+}
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <el-button>
+  <el-button @click="showMessage">
     <span class="less"> less </span>
   </el-button>
 </template>
