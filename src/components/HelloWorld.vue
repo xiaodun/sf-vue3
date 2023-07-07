44197c2b2fb69f7cf7ae83bdd0f50491288f1ac0
Xiao dun <1947147481@qq.com>
2023-07-07 10:57:35 +0800

    使用@别名

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useLoginStore } from "../store";
+import { useLoginStore } from "@/store";
 import {
   ref,
   shallowReactive,
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     AutoImport({
       resolvers: [ElementPlusResolver()],
